789cdbfcf90dbde34c2bcaa5e2fc733b5e479c78
jiaenlow-rp <21024625@myrp.edu.sg>
2023-01-03 15:53:58 +0800

    more updates to test.php

--- web/test.php
+++ web/test.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+<title>
+</title>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="a page with the word Hello World! on it this is to greet the user opening this page.">
 </head>
 <body>
   Hello World!
