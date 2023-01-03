5e4c69bd91bd7a2b46e7efea0d993dfdbf6c336a
jiaenlow-rp <21024625@myrp.edu.sg>
2023-01-03 16:19:46 +0800

    added ex 4 test2.php

---
+++ web/test2.php
@@ -0,0 +1,13 @@
+<img alt="Kim banner img" class="tp--carousel-img center-align-caro" src="/content/dam/tp-web/images/home/banner/Kimberly_PET_web.jpg">
+ 
+<img alt="Adam banner img" class="tp--carousel-img center-align-caro" src="/content/dam/tp-web/images/home/banner/Adam_CET_web.jpg">
+ 
+<img alt="tp focused study img" src="/content/dam/tp-web/images/home/tiles/TP%20Focused%20Holistic%20Approach.jpg" style="height: 100%; width: auto;">
+ 
+<img alt="lifelong learning img" src="/content/dam/tp-web/images/home/tiles/TP%20Lifelong%20Learning.jpg" style="height: 100%; width: auto;">
+ 
+<img alt="no bg human img" src="/content/dam/tp-web/images/home/quicklinks/home-quicklinks-background.png">
+ 
+<img alt="speech bubble icon" src="/etc.clientlibs/tp-web/clientlibs/clientlib-site/resources/images/chat-icon.png">
+ 
+<img alt="to the top icon" src="/etc.clientlibs/tp-web/clientlibs/clientlib-site/resources/images/to-top.png">
